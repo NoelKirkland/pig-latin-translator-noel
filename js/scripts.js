7b2e3847df21d3ca216e0a21f84c20890b318537
Noel Kirkland <noelkirkland@gmail.com>
2020-06-10 16:57:37 -0700

    working out the kinks
    co-authored-by Jason Khan <jasonderekkhan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,9 @@
-// // Business logic
-// function pigLatinTranslator(splitSentence){
-//   for (let i=0; splitSentence.length > i; ++i) {
-//     let splitCharacter = splitSentence.split("");
-//       if (splitCharacter[i] !== vowels) {
-//         return true;
-//}
-//   };
-// };
+
+const vowels = ["a", "e", "i", "o", "u"];
+const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y']
+
+
+
 
 // User interface
 $(document).ready(function(){
@@ -14,33 +11,17 @@ $(document).ready(function(){
     event.preventDefault();
     let inputSentence = $("input#input-sentence").val();
     let splitSentence = inputSentence.split(" ");
-    let splitCharacter = splitSentence.join(" ").split("");
-    // let splitCharacter2 = inputSentence.split("");
-    const vowels = ["a", "e", "i", "o", "u"];
-    let returnString = "";
-      if(vowels.indexOf(inputSentence [0])>1) {
-        returnString = inputSentence + "way";
-        return returnString;
-      }
-      else {
-        let firstMatch = inputSentence.match(/[aeiou]/g) || 0;
-        let vowel = inputSentence.indexOf(firstMatch[0]);
-        returnString = inputSentence.substring(vowel) + inputSentence.substring(0, vowel) + "ay";
-        return returnString;
+    let splitCharacter = inputSentence.split("");
+    for (let i=0; splitSentence.length > i; ++i) {
+      if (splitSentence[i].length === 1) {
+        splitSentence[i] += "way";
+        console.log(splitSentence[i]);
+        } else if (consonants.indexOf(splitSentence[0])) {
+            splitSentence[i] += "way";
+            console.log(splitSentence[i]);
+        }
     }
-      
-  }); 
+//    let splitCharacter = splitSentence.join(" ").split("");
+
+  });
 });
-
-// for (let i = 0; i <= splitSentence.length; i++) {
-//   if (splitSentence[i] === a)
-//   return true;
-// }
-
-// let splitCharacters = [];
-// for (const element of array) {
-//   splitCharacters.split(element);
-
-  // for (let index = 0; index <= splitCharacter.length; index++){
-
-  // }
---
+++ test.js
@@ -0,0 +1,40 @@
+// // // Business logic
+// // function pigLatinTranslator(splitSentence){
+// //   for (let i=0; splitSentence.length > i; ++i) {
+// //     let splitCharacter = splitSentence.split("");
+// //       if (splitCharacter[i] !== vowels) {
+// //         return true;
+// //}
+// //   };
+// // };
+
+// // User interface
+// $(document).ready(function(){
+//   $("form#pig-latin").submit(function(event){
+//     event.preventDefault();
+//     let inputSentence = $("input#input-sentence").val();
+//     let splitSentence = inputSentence.split(" ");
+//     // let splitCharacter = splitSentence.join(" ").split("");
+//     let splitCharacter2 = inputSentence.split("");
+//     const vowels = ["a", "e", "i", "o", "u"];
+//     const consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+//     let returnString = "";
+//       if (vowels.indexOf(splitCharacter2[0]) >= 0 && consonants.indexOf(splitCharacter2[1]) >= 1) {
+//         return (splitCharacter2.join("")).concat(splitCharacter2 + "way");
+//       }
+//   }); 
+// });
+
+// // for (let i = 0; i <= splitSentence.length; i++) {
+// //   if (splitSentence[i] === a)
+// //   return true;
+// // }
+
+// // let splitCharacters = [];
+// // for (const element of array) {
+// //   splitCharacters.split(element);
+
+//   // for (let index = 0; index <= splitCharacter.length; index++){
+
+//   // }
+
